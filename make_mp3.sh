341993a3019c24281d5a713c21da8058387110f1
mrsmmori1234 <mrsmmori@gmail.com>
2026-03-18 18:18:18 +0800

    Mar18

--- make_mp3.sh
+++ make_mp3.sh
@@ -2,7 +2,7 @@
 
 # 1. 引数があるかチェック
 if [ $# -eq 0 ]; then
-    echo "使用法: ./combine.sh [ディレクトリパス]"
+    echo "使用法: ./make_mp3.sh [ディレクトリパス]"
     exit 1
 fi
 
@@ -17,7 +17,7 @@ fi
 # 3. Pythonスクリプトの実行
 # combine.py が同じディレクトリにあることを想定しています
 echo "処理を開始します: $TARGET_DIR"
-/home/mrsmmori/.pyenv/versions/scripts-env/bin/python /home/mrsmmori/scripts/combine.py $TARGET_DIR
+/home/mrsmmori/.pyenv/versions/scripts-env/bin/python /home/mrsmmori/scripts/make_mp3.py $TARGET_DIR
 
 
 # 4. 実行結果の確認
